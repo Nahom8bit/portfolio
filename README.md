87286b9637421f12279cc48db02c13ef5b9d50fd
Their Name <nahomryu@gmail.com>
2024-09-08 16:44:41 +0100

    main

--- README.md
+++
@@ -1,2 +0,0 @@
-# portfolio
-My Projects portfolio
--- frontend/script.js
+++
@@ -1,4 +0,0 @@
-fetch('/api/hello')
-    .then(response => response.text())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
